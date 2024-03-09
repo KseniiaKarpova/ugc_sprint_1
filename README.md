9e05111f3b38e2a59bbb66b0aa3edccd170bdb38
kseniia <ksusha.karpova2011@yandex.ru>
2024-03-09 19:16:25 +0300

    update readme and docker

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Проектная работа 8 спринта
 
+link to git = https://github.com/KseniiaKarpova/ugc_sprint_1
+
+
 ### Описание проекта:
 - `task_0` - Описание архитектуры с использованием PlantUML.
 
---
+++ task_1/data/init-db/init-shard1.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS shard;
+CREATE DATABASE IF NOT EXISTS replica;
+
+CREATE TABLE IF NOT EXISTS shard.events (
+      id UUID DEFAULT generateUUIDv4(),
+      user_id UUID,
+      film_id UUID,
+      action String,
+      created_at DateTime('Europe/Moscow')
+      )
+      Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/events', 'replica_1') PARTITION BY toYYYYMMDD(created_at) ORDER BY id;
+
+CREATE TABLE IF NOT EXISTS replica.events (
+      id UUID DEFAULT generateUUIDv4(),
+      user_id UUID,
+      film_id UUID,
+      action String,
+      created_at DateTime('Europe/Moscow')
+      )
+      Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/events', 'replica_2') PARTITION BY toYYYYMMDD(created_at) ORDER BY id;
+
+CREATE TABLE IF NOT EXISTS default.events (
+      id UUID DEFAULT generateUUIDv4(),
+      user_id UUID,
+      film_id UUID,
+      action String,
+      created_at DateTime('Europe/Moscow')
+      )
+      ENGINE = Distributed('company_cluster', '', events, rand());
