15aa4a2ead8f63ebfb83f9be9499d494757424a3
NozirTukhtamurodov <nozir9407@gmail.com>
2024-03-12 09:18:40 +0500

    AUTH added

---
+++ task_1/UGC/services/producer.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+from uuid import UUID
+from fastapi import Depends
+from aiokafka import AIOKafkaProducer
+from db.kafka import get_kafka
+from schemas.action import UserAction
+from utils.jaeger import TraceAction
+from schemas.auth import JWTUserData
+from core.config import settings
+from exceptions import success
+from datetime import datetime
+
+class ProducerService:
+    def __init__(self, producer: AIOKafkaProducer) -> None:
+        self.producer = producer
+
+    @TraceAction('create_action_execution')
+    async def produce(self, user: JWTUserData, film_id: UUID, action: str):
+        data = UserAction(
+            action=action,
+            user_id=str(user.uuid),
+            film_id=str(film_id),
+            created_at=datetime.utcnow()
+            ).model_dump_json().encode('utf-8')
+        await self.producer.send(topic=settings.kafka.topic, value=data)
+        return success
+
+
+@lru_cache()
+def get_producer_service(
+    producer: AIOKafkaProducer = Depends(get_kafka),
+    ):
+    return ProducerService(producer=producer)
